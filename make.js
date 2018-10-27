0e62673a9402c4a9504efed9128a593cb91f98cb
Mikalai Hasiukevich <mike80by@gmail.com>
2018-10-28 01:45:47 +0300

    feat: add solution function#2 make

---
+++ make.js
@@ -0,0 +1,21 @@
+function make(...params) {
+	let arrSum = [];
+	arrSum.push(...params);
+	const inFunc = function(...params) {
+		for (let i = 0, len = params.length; i < len; i++) {
+			if (typeof params[i] === "number")  {
+				arrSum.push(params[i]);
+			} else if (typeof params[i] === "function") {
+				inFunc.valueOf = function() {
+					return arrSum.reduce(sum);
+				}
+			}
+		}
+		return inFunc;
+	}
+	return inFunc;
+}
+
+function sum(a, b) {
+	return a + b;
+}
